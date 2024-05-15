a37eed68b3e2b07081875985f680e3fb9070c752
Ody Machairas <o.machairas@student.tudelft.nl>
2024-05-16 00:13:12 +0200

    feat: Added admin page to populate db with sample data

---
+++ frontend/src/routes/admin/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<main class="mx-auto max-w-xl py-8">
+	<h1 class="text-2xl font-bold">Admin dashboard</h1>
+
+	<form method="post" action="?/seedSampleTorgo">
+		<Button type="submit">Seed database with TORGO sample data</Button>
+	</form>
+</main>
