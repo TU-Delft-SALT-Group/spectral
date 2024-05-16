5752bbf5f206abff4c7aea6a8b057a0141209065
Yousef El Bakri <Y.ElBakri-1@student.tudelft.nl>
2024-05-16 22:33:24 +0200

    feat(waveform): added very basic visualization

--- frontend/src/lib/analysis/modes/waveform/Waveform.svelte
+++ frontend/src/lib/analysis/modes/waveform/Waveform.svelte
@@ -1,8 +1,39 @@
 <script lang="ts">
+	import WaveSurfer from 'wavesurfer.js';
 	import type { SpecificModeData } from '..';
+	import { onMount } from 'svelte';
 
-	// eslint-disable-next-line
 	export let data: SpecificModeData<'waveform'>[];
+
+	let lastSelected: WaveSurfer | null = null;
+
+	function keybinds(e: KeyboardEvent) {
+		switch (e.key) {
+			case ' ':
+				if (lastSelected === null) return;
+				lastSelected.playPause();
+				break;
+		}
+	}
+
+	onMount(() =>
+		data.forEach((item) => {
+			let wavesurfer = new WaveSurfer({
+				container: `#${item.fileId}-waveform`,
+				url: `/db/${item.fileId}`
+			});
+
+			wavesurfer.on('interaction', () => {
+				lastSelected = wavesurfer;
+			});
+		})
+	);
 </script>
 
-<!-- Write code here using `data` -->
+<section class="w-full pr-20">
+	{#each data as item}
+		<div id={`${item.fileId}-waveform`}></div>
+	{/each}
+</section>
+
+<svelte:window on:keydown={keybinds} />
