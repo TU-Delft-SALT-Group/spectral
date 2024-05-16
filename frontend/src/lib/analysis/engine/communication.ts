58cf4444cbb333a0e5d218e40ff4e20828cd882b
Ody Machairas <o.machairas@student.tudelft.nl>
2024-05-16 15:33:31 +0200

    feat: query engine from sveltekit

--- frontend/src/lib/analysis/engine/communication.ts
+++ frontend/src/lib/analysis/engine/communication.ts
@@ -2,27 +2,24 @@
  * Entry point for communicating with the Python engine
  */
 
-import { env } from '$env/dynamic/public';
-import { unwrap } from '$lib/utils';
 import { modeDataValidator, type Mode, type ModeData } from '$lib/analysis/modes';
 import type { Frame } from '$lib/analysis/engine/framing';
 import { error } from '@sveltejs/kit';
-
-const ORIGIN = unwrap(
-	env.PUBLIC_KERNEL_ORIGIN,
-	'Python kernel origin not given (example: `https://192.168.0.5:6942`, without a trailing slash)'
-);
+import { browser } from '$app/environment';
 
 /**
  * Main way to fetch stuff for backend
  */
 export function getURL(path: string): URL {
+	if (!browser) {
+		throw Error('This function is only available in the browser');
+	}
 	// Remove leading slash
 	if (path.startsWith('/')) {
 		path = path.slice(1);
 	}
 
-	return new URL(`${ORIGIN}/${path}`);
+	return new URL(`api/${path}`, window.location.origin);
 }
 
 /**
