91d8c3e2bb0cf93d771cc5a9dd7ced776365e5b4
Yousef El Bakri <Y.ElBakri-1@student.tudelft.nl>
2024-05-16 23:13:11 +0200

    feat(waveform): deselection/selection is visible

--- frontend/src/lib/analysis/modes/waveform/Waveform.svelte
+++ frontend/src/lib/analysis/modes/waveform/Waveform.svelte
@@ -4,14 +4,36 @@
 	import { onMount } from 'svelte';
 
 	export let data: SpecificModeData<'waveform'>[];
-
 	let lastSelected: WaveSurfer | null = null;
+
+	type waveColour = {
+		wave: string;
+		progress: string;
+	};
+
+	const selected: waveColour = {
+		wave: '#aaaaaa',
+		progress: '#ffffff'
+	};
+
+	const other: waveColour = {
+		wave: '#444444',
+		progress: '#999999'
+	};
 
 	function keybinds(e: KeyboardEvent) {
 		switch (e.key) {
 			case ' ':
 				if (lastSelected === null) return;
 				lastSelected.playPause();
+				break;
+			case 'Escape':
+				lastSelected?.setOptions({
+					waveColor: other.wave,
+					progressColor: other.progress
+				});
+
+				lastSelected = null;
 				break;
 		}
 	}
@@ -20,11 +42,25 @@
 		data.forEach((item) => {
 			let wavesurfer = new WaveSurfer({
 				container: `#${item.fileId}-waveform`,
-				url: `/db/${item.fileId}`
+				url: `/db/${item.fileId}`,
+				waveColor: other.wave,
+				progressColor: other.progress
 			});
 
 			wavesurfer.on('interaction', () => {
+				if (lastSelected) {
+					lastSelected.setOptions({
+						progressColor: other.progress,
+						waveColor: other.wave
+					});
+				}
+
 				lastSelected = wavesurfer;
+
+				lastSelected.setOptions({
+					progressColor: selected.progress,
+					waveColor: selected.wave
+				});
 			});
 		})
 	);
