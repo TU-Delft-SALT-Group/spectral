4a63ae044aa023142f05b07ee94b7dda267c834f
Ody Machairas <o.machairas@student.tudelft.nl>
2024-05-19 15:19:31 +0200

    fix(app): Cached to make mode selector snappier

--- frontend/src/lib/analysis/modes/ModeSelector.svelte
+++ frontend/src/lib/analysis/modes/ModeSelector.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
+	import { flip } from 'svelte/animate';
 	import { getIcon, modes, type Mode } from '.';
 
 	export let mode: Mode;
-
-	$: selectedIndex = modes.indexOf(mode);
+	export let onModeHover: (mode: Mode) => void = () => {};
 </script>
 
 <div class="main absolute right-4 top-4 z-30 flex flex-col gap-2 transition-all">
-	{#each modes as currentMode, i}
+	{#each modes as currentMode, i (currentMode)}
 		<div
 			class:z-40={mode === currentMode}
-			style:--index={i - selectedIndex}
+			style:--index={i}
 			class:opacity-0={mode !== currentMode}
 			class="select relative transition duration-500"
+			animate:flip={{ delay: 1000 }}
 		>
 			<Button
 				on:click={() => (mode = currentMode)}
+				on:hover={() => onModeHover(currentMode)}
 				variant={mode === currentMode ? 'default' : 'ghost'}
 				class="h-10 w-16 shadow"
 			>
@@ -37,6 +39,11 @@
 		transform: translateY(calc(-100% * var(--index)));
 		transition-timing-function: cubic-bezier(0.86, 0, 0.07, 1); /* exponential-ish in out */
 		transition-delay: 0.3s;
+		pointer-events: all;
+	}
+
+	.main {
+		pointer-events: none;
 	}
 
 	.main:hover > .select {
