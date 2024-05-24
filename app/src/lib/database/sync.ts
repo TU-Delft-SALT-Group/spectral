64bf9e76fde7d833d3627a23e8b7f25f2577cda6
Yousef El Bakri <Y.ElBakri-1@student.tudelft.nl>
2024-05-24 14:15:23 +0200

    fix: remove unused file

--- app/src/lib/database/sync.ts
+++
@@ -1,7 +0,0 @@
-import type { PaneState } from '$lib/analysis/analysis-pane';
-import used from '$lib/utils';
-
-export async function syncPaneStateToDb(state: PaneState) {
-	used(state);
-	// console.log('Should be syncing state', state);
-}
