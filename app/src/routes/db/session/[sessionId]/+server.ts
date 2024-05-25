67f131a7850aa4faecf709b1fbc047e547d6acbe
Yousef El Bakri <Y.ElBakri-1@student.tudelft.nl>
2024-05-25 09:48:18 +0200

    fix: state not syncing to database correctly

--- app/src/routes/db/session/[sessionId]/+server.ts
+++ app/src/routes/db/session/[sessionId]/+server.ts
@@ -2,16 +2,22 @@ import { db } from '$lib/database';
 import { sessionTable } from '$lib/database/schema';
 import { error, type RequestHandler } from '@sveltejs/kit';
 import { sessionState, type SessionState } from '../../../session/[sessionId]/workspace';
+import { eq } from 'drizzle-orm';
 
 export const POST: RequestHandler = async ({ params, request }) => {
 	const json = await request.json();
 	const val: SessionState = sessionState.parse(json);
+	if (params.sessionId === undefined) throw error(400, `Failed to be given a session ID.`);
+
+	const id: string = params.sessionId;
 
 	try {
-		await db.update(sessionTable).set({
-			id: params.sessionId,
-			...val
-		});
+		await db
+			.update(sessionTable)
+			.set({
+				state: val
+			})
+			.where(eq(sessionTable.id, id));
 	} catch (e) {
 		throw error(400, `Failed with error message: ${e}.`);
 	}
