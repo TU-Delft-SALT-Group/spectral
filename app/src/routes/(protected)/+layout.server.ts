1f7c72c17b9607b56208f52d18eafca28cecf4be
Ody Machairas <o.machairas@student.tudelft.nl>
2024-05-30 14:52:37 +0200

    fix(app): Go to visiting page after login

--- app/src/routes/(protected)/+layout.server.ts
+++ app/src/routes/(protected)/+layout.server.ts
@@ -1,10 +1,14 @@
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async ({ parent }) => {
+export const load: LayoutServerLoad = async ({ parent, url, cookies }) => {
 	const { user } = await parent();
 
 	if (user === null) {
+		cookies.set('redirect-after-login', url.toString(), {
+			path: '/'
+		});
+
 		redirect(302, '/login');
 	}
 
