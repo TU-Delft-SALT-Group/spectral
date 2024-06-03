f0dc0b16d26ae16a38518242c3073387b86246bc
Yousef El Bakri <Y.ElBakri-1@student.tudelft.nl>
2024-06-03 16:47:14 +0200

    fix: fix errors from `pnpm check`

--- app/src/lib/analysis/modes/transcription/TranscriptionIcon.svelte
+++ app/src/lib/analysis/modes/transcription/TranscriptionIcon.svelte
@@ -1,5 +1,8 @@
 <script>
 	import { TextCursorInputIcon } from 'lucide-svelte';
+
+	// Using `export let` opts out of runes mode
+	export const disableRunes = true;
 </script>
 
 <TextCursorInputIcon />
