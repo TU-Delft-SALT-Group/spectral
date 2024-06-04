2328cff532eed75b1de289df8db75406002e64bb
Yousef El Bakri <Y.ElBakri-1@student.tudelft.nl>
2024-06-04 18:03:27 +0200

    feat: very basic transcription

--- app/src/lib/analysis/modes/file-state.ts
+++ app/src/lib/analysis/modes/file-state.ts
@@ -1,29 +1,29 @@
 import { z } from 'zod';
 import { frame } from '../kernel/framing';
 
-export const transcriptions = z
-	.array(
-		z.object({
-			id: z.string().default('broken-track-id'),
-			name: z.string().default('broken-track-name'),
-			captions: z.array(
+export const transcription = z
+	.object({
+		id: z.string().default('broken-track-id'),
+		name: z.string().default('broken-track-name'),
+		captions: z
+			.array(
 				z.object({
 					value: z.string(),
 					start: z.number(),
 					end: z.number()
 				})
 			)
-		})
-	)
-	.default([]);
+			.default([])
+	})
+	.default({});
 
 export const fileState = z.object({
 	id: z.string().default('broken-file-id'),
 	name: z.string().default('broken-filename'),
 	frame: frame.nullable().default(null),
 	cycleEnabled: z.boolean().default(false),
-	transcriptions
+	transcriptions: z.array(transcription).default([])
 });
 
 export type FileState = z.infer<typeof fileState>;
-export type Transcriptions = z.infer<typeof transcriptions>;
+export type Transcription = z.infer<typeof transcription>;
