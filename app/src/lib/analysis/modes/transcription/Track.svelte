5d2ed2b5b65ea085e1c1fdac5dee30f412a9a76a
Yousef El Bakri <Y.ElBakri-1@student.tudelft.nl>
2024-06-05 14:39:55 +0200

    feat: saving when changing a caption's length

--- app/src/lib/analysis/modes/transcription/Track.svelte
+++ app/src/lib/analysis/modes/transcription/Track.svelte
@@ -1,12 +1,32 @@
 <script lang="ts">
 	import type { Transcription } from '../file-state';
 	import * as Resizable from '$lib/components/ui/resizable';
-	import type { PaneGroupAPI } from 'paneforge';
 	import { Button } from '$lib/components/ui/button';
+	import type { PaneGroupAPI } from 'paneforge';
 
-	export let transcription: Transcription;
-	export let duration: number;
-	let paneGroup: PaneGroupAPI;
+	let {
+		transcription,
+		duration
+	}: {
+		transcription: Transcription;
+		duration: number;
+	} = $props();
+
+	let paneGroup: PaneGroupAPI | undefined = $state(undefined);
+	let handleElementList: HTMLElement[] = $state([]);
+	let currentlyHeldButton: HTMLElement | null = null;
+
+	$effect(() => {
+		if (handleElementList === null) {
+			return;
+		}
+
+		for (const element of handleElementList) {
+			element.onmousedown = () => {
+				currentlyHeldButton = element;
+			};
+		}
+	});
 
 	type Caption = {
 		start: number;
@@ -20,7 +40,6 @@
 
 		let boundingBox = (event.target! as HTMLElement).getBoundingClientRect();
 		let percentage = (event.x - boundingBox.left) / boundingBox.width;
-
 		let oldEnd = caption.end;
 		let ind = transcription.captions.indexOf(caption);
 
@@ -29,7 +48,7 @@
 		let newCaption: Caption = {
 			start: caption.end,
 			end: oldEnd,
-			value: 'work'
+			value: ''
 		};
 
 		transcription.captions = [
@@ -38,47 +57,50 @@
 			...transcription.captions.slice(ind + 1)
 		];
 	}
-
-	function updateSizes(captions: Caption[]) {
-		if (paneGroup === undefined || duration === undefined) {
-			return;
-		}
-
-		let percentages = captions.map(({ start, end }) => (end - start) / duration);
-		paneGroup.setLayout(percentages);
-	}
-
-	$: updateSizes(transcription.captions);
 </script>
 
 <div class="flex w-full flex-row items-center gap-4">
 	<span>{transcription.name}</span>
-	<Resizable.PaneGroup
-		direction="horizontal"
-		class="w-full"
-		onLayoutChange={(layout) => {
-			if (layout.length !== transcription.captions.length) {
-				return;
-			}
-
-			console.log(layout);
-		}}
-		bind:paneGroup
-	>
+	<Resizable.PaneGroup direction="horizontal" class="w-full" bind:paneGroup>
 		{#if duration !== undefined}
-			{#each transcription.captions as caption ([caption.start, caption.end])}
-				<Resizable.Pane defaultSize={(caption.start - caption.end) / duration}>
+			{#each transcription.captions as caption, i ([caption.start, caption.end])}
+				<Resizable.Pane class="w-full" defaultSize={(caption.start - caption.end) / duration}>
 					<Button
-						class="w-full"
+						class="w-full rounded-none"
 						variant="outline"
 						onclick={(event: MouseEvent) => onClick(event, caption)}>{caption.value}</Button
 					>
 				</Resizable.Pane>
 
 				{#if caption !== transcription.captions[transcription.captions.length - 1]}
-					<Resizable.Handle />
+					<Resizable.Handle bind:el={handleElementList[i]}></Resizable.Handle>
 				{/if}
 			{/each}
 		{/if}
 	</Resizable.PaneGroup>
 </div>
+
+<svelte:window
+	on:mouseup={(e) => {
+		if (currentlyHeldButton === null || paneGroup === undefined) {
+			return;
+		}
+
+		e.preventDefault();
+
+		let captions = transcription.captions;
+		let layout = paneGroup.getLayout();
+		let prevEnd = 0;
+
+		for (let i = 0; i < layout.length; i++) {
+			captions[i].start = prevEnd;
+			captions[i].end = prevEnd + duration * (layout[i] / 100);
+			prevEnd = captions[i].end;
+		}
+
+		transcription.captions = captions;
+		currentlyHeldButton = null;
+
+		return;
+	}}
+/>
