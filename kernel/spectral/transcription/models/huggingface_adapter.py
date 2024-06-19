7db4c490fccd0afcb1b78a9fd767c677865c3034
Roman Knyazhitskiy <git@knyaz.tech>
2024-06-19 20:21:56 +0200

    fix: conversion issues of audio format

--- kernel/spectral/transcription/models/huggingface_adapter.py
+++ kernel/spectral/transcription/models/huggingface_adapter.py
@@ -1,7 +1,10 @@
 """Interface to load arbitrary* HF models."""
 
+from collections.abc import Callable
 from functools import lru_cache
+from pathlib import Path
 
+import numpy as np
 import torch
 from transformers import WhisperForConditionalGeneration, WhisperProcessor
 
@@ -10,7 +13,7 @@ from spectral.types import TranscriptionType
 
 
 @lru_cache
-def _get_model_by_name(model_name):
+def _get_model_by_name(model_name: str) -> tuple:
     if model_name != "torgo":
         raise RuntimeError("""We don't support any other model besides
                            'torgo' one for now: some models on huggingface
@@ -18,33 +21,37 @@ def _get_model_by_name(model_name):
                            is the jindaznb/torgo_tiny_finetune_F01_frozen_encoder
                            patched with tiny whisper tokenizer...""")
 
-    model = WhisperForConditionalGeneration.from_pretrained(model_name, local_files_only=True)
-    processor = WhisperProcessor.from_pretrained(model_name, local_files_only=True)
+    path = Path(__file__).parent / Path(model_name)
+    model = WhisperForConditionalGeneration.from_pretrained(path, local_files_only=True)
+    processor = WhisperProcessor.from_pretrained(path, local_files_only=True)
     return (model, processor)
 
 
 @lru_cache
-def get_transcribe_fn(model_name):
+def get_transcribe_fn(model_name: str) -> Callable[[bytes], str]:
     """Return a complete prediction function, that adheres to the usual API."""
     model, processor = _get_model_by_name(model_name)
     required_sr = 16000
 
-    def transcribe_fn(data):
-        input_features = processor(  # type: ignore
+    def transcribe_fn(data: bytes) -> str:
+        audio = signal_analysis.get_audio({"data": data})
+        # trick from https://github.com/openai/whisper/discussions/983
+        data = np.frombuffer(audio.raw_data, np.int16).flatten().astype(np.float32) / 32768.0  # type: ignore
+        input_features = processor(
             data, sampling_rate=required_sr, return_tensors="pt"
-        ).input_features
+        ).input_features  # type: ignore
 
         # Generate transcription
         with torch.no_grad():
             predicted_ids = model.generate(input_features)  # type: ignore
-            return processor.batch_decode(predicted_ids, skip_special_tokens=False)  # type: ignore
+            return processor.batch_decode(predicted_ids, skip_special_tokens=True)[0]  # type: ignore
 
     return transcribe_fn
 
 
 def hf_transcription(data: bytes, model_name: str) -> TranscriptionType:
     """
-    Get transcription from whisper from an list of WAV bytes.
+    Get transcription from hf given wav bytes representation.
 
     Args:
     ----
