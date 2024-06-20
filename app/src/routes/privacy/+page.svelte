ecd6545621ffe3b7069418e4d00c19a6961d759c
Yousef El Bakri <Y.ElBakri-1@student.tudelft.nl>
2024-06-20 17:43:51 +0200

    feat: privacy policy page

---
+++ app/src/routes/privacy/+page.svelte
@@ -0,0 +1,35 @@
+<svelte:head>
+	<title>Privacy Policy</title>
+</svelte:head>
+
+<section class="h-full w-screen break-words px-[15%]">
+	<h1 class="flex w-full justify-center text-2xl">Consumer Data</h1>
+
+	<p>
+		Internally, audio/video files are stored in the TU Delft servers, where they are properly
+		secured by the using a hashed ID, this means people with access to this ID have access to the
+		files.
+	</p>
+
+	<p>
+		In addition, users comply that anyone that has access to the server <i>also</i> has access to all
+		of the users data that is stored.
+	</p>
+
+	<h1 class="flex w-full justify-center text-2xl">Data going to external services</h1>
+
+	<p>While we are open source, we use services for transcription that are not. These are:</p>
+	<ul class="ml-5 list-disc">
+		<li>
+			<a href="https://openai.com/policies/eu-privacy-policy/" class="text-blue-300"
+				>OpenAI Whisper</a
+			>
+		</li>
+		<li><a href="https://deepgram.com/privacy" class="text-blue-300">Deepgram</a></li>
+	</ul>
+	<p>
+		By using our services, users comply with their privacy policies. <i>Please</i> go to their website
+		and read their policies.
+	</p>
+	<p>Data does not get shared to any other services other than those listed above.</p>
+</section>
